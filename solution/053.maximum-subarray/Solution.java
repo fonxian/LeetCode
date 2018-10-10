bae10b7f24744c5b8c2be310c709a7c5809c9ba1
fonxian <fonxian2015@163.com>
2018-10-11 03:51:54 +0800

    add 053 maximum-subarray

---
+++ solution/053.maximum-subarray/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+
+    public static int climbStairs(int n) {
+
+        int[] result = new int[n+2];
+
+        result[0] = 0;
+        result[1] = 1;
+        result[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+      
+            result[i]+= result[i-1] + result[i-2];
+            
+        }
+        return result[n];
+
+    }
+
+}
